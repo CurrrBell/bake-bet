2a298c4e1992f13eaa25a4d85f3005364402125e
Chris Bell <chrislbellny@gmail.com>
2025-12-19 13:16:18 -0600

    working mock auth

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,18 +1,24 @@
 <template>
-    <nav>nav</nav>
+    <nav v-if="isLoggedIn">nav</nav>
     <main>
         <RouterView />
     </main>
 </template>
 <script setup lang="ts">
-    import { RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
+
+import { useUserStore } from './stores/user';
+import { storeToRefs } from 'pinia';
+
+const { isLoggedIn } = storeToRefs(useUserStore());
 </script>
 <style scoped>
     nav {
-        grid-area: nav
+        grid-area: nav;
     }
 
     main {
-        grid-area: main
+        grid-area: main;
+            display: flex;
     }
 </style>
--- packages/web/src/stores/user.ts
+++ packages/web/src/stores/user.ts
@@ -2,13 +2,24 @@ import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
 export const useUserStore = defineStore('user', () => {
-    const id = ref(null);
+    const id = ref('');
     const name = ref('');
     const balance = ref(0);
 
     const isLoggedIn = computed(() => !!id.value);
 
+    function login(newId: string, newName: string, newBalance: number) {
+        return Promise.resolve()
+            .then(() => {
+                id.value = newId;
+                name.value = newName;
+                balance.value = newBalance
+            })
+
+    }
+
     return {
-        isLoggedIn
+        isLoggedIn,
+        login
     }
 });
