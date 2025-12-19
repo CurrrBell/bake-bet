66d2cce774dd7068115e79c76f30f2fb4d35e690
Chris Bell <chrislbellny@gmail.com>
2025-12-19 16:18:43 -0600

    ditch vuetify, do our own css

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-    <nav v-if="isLoggedIn">nav</nav>
+    <main-nav v-if="isLoggedIn">nav</main-nav>
     <main>
         <RouterView />
     </main>
 </template>
 <script setup lang="ts">
+import MainNav from './components/MainNav.vue';
 import { RouterView } from 'vue-router';
 
 import { useUserStore } from './stores/user';
@@ -13,12 +14,11 @@ import { storeToRefs } from 'pinia';
 const { isLoggedIn } = storeToRefs(useUserStore());
 </script>
 <style scoped>
-    nav {
-        grid-area: nav;
-    }
+nav {
+    grid-area: nav;
+}
 
-    main {
-        grid-area: main;
-            display: flex;
-    }
+main {
+    grid-area: main;
+}
 </style>
